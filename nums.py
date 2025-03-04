32177d0e98a98c5ec575b61624b6c128624b7ef3
LydiaFrame <156206633+LydiaFrame@users.noreply.github.com>
2025-03-04 03:37:38 +0000

    finish

---
+++ nums.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""Program to read positive numbers and print sorted data."""
+
+__author__ = "Lydia Frame"
+__date__ = "03/01/2025"
+
+def main():
+    """Main function to collect numbers and display sorted results."""
+    numbers = []
+    evens = []
+    odds = []
+
+    while True:
+        try:
+            num = int(input("Num? "))  
+            print()
+            
+            if num < 0:
+                break 
+            
+            numbers.append(num)
+            
+            if num % 2 == 0:
+                evens.append(num)
+            else:
+                odds.append(num)
+                
+        except ValueError:
+            print("Invalid input! Please enter an integer.")
+
+    print("\nAll (sorted):", sorted(numbers, reverse=True))
+    print("Evens:", evens)
+    print("Odds:", odds)
+
+if __name__ == "__main__":
+    main()
